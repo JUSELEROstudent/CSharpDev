1f08c1a9292d9df57f6f79598694e20891ca2024
JUSELERO <juan.leon7@correo.uis.edu.co>
2023-12-20 23:18:13 -0500

    se agrega chapter 11 sin funcionar primer ejemplo

---
+++ Chapter11/WorkingWithEFCore/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace ConsoleApp
+{
+  static class AppConsole
+  {
+    public static void Main()
+    {
+      QueryingCategorie();
+    }
+    static void QueryingCategorie()
+    {
+      using (var db = new Northwind())
+      {
+        WriteLine("cATEGORIES AND HOW MANY ProductS THEY HAVE");
+        IQueryable<Category> cats = db.Categories.Include(c=> c.Products);
+        WriteLine(cats);
+        foreach (Category c in cats)
+        {
+          WriteLine($"{c.CategoryName} has  {c.Products.Count} products");
+        }
+      }
+    }
+  }
+
+}
